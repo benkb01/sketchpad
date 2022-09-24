bedce3ffb9150de2ce3ee90a37a98d13f2974cbb
benkb01 <benkb061205@icloud.com>
2022-09-24 08:41:38 -0400

    Add multiple squares User can change size

--- js/sketchpad.js
+++ js/sketchpad.js
@@ -1,22 +1,50 @@
-const changeSizeBtn = document.querySelector('changeSize');
-//changeSizeBtn.addEventListener('click', );
+let length =16;
+
+const changeSizeBtn = document.querySelector('#changeSize');
+changeSizeBtn.addEventListener('click', newGrid);
 
 const container = document.querySelector('#container');
-const square = document.createElement('div');
-square.style.opacity = 0;
-square.style.backgroundColor = 'black';
-square.style.borderStyle = 'solid';
-square.style.width = '100px';
-square.style.height = '100px';
-square.addEventListener('mouseover', function() {
-    temp = getOpacity(square.style.opacity);
-    square.style.opacity = temp;
-    
-});
-
+container.style.lineHeight = 0;
+makeGrid();
 
 container.appendChild(square);
 
+function newGrid()
+    {
+    clearGrid();
+    setLength();
+    }
+
+function makeGrid()
+    {
+    
+    for (let i = 0; i < length * length; i++)
+        {
+        let square = document.createElement('div');
+        square.style.opacity = 0;
+        square.style.backgroundColor = 'black';
+        square.style.width = (Math.floor(400.0 / length)).toString() + 'px';
+        square.style.height = (Math.floor(400.0 / length)).toString() + 'px';
+        square.style.display = 'inline-block';
+
+        square.classList.add('square');
+
+        square.addEventListener('mouseover', function(e) {
+        e.target.style.opacity = getOpacity(e.target.style.opacity);
+        });
+
+        container.appendChild(square);
+        }
+    }
+
+function clearGrid()
+    {
+    let squares = document.querySelectorAll('.square');
+    squares.forEach((div) => 
+        {
+        div.parentElement.removeChild(div);
+        });
+    }
 
 function getOpacity(n)
     {
@@ -33,7 +61,7 @@ function getOpacity(n)
         }
     }
 
-function getLength() 
+function setLength() 
     {
     temp = parseInt(prompt("Enter Side Length, please"));
     if (temp < 2)
@@ -44,7 +72,8 @@ function getLength()
         {
         temp = 100;
         }
-
+    length = temp;
+    makeGrid();
     }
 
 function getColor(color, opacity)
@@ -52,3 +81,19 @@ function getColor(color, opacity)
     
         
     }
+
+
+    /*
+const square = document.createElement('div');
+
+square.style.opacity = 0;
+square.style.backgroundColor = 'black';
+square.style.borderStyle = 'solid';
+square.style.width = (Math.floor(400.0 / length)).toString() + 'px';
+square.style.height = (Math.floor(400.0 / length)).toString() + 'px';
+square.style.display = 'inline-block';
+
+square.addEventListener('mouseover', function(e) {
+    e.target.style.opacity = getOpacity(e.target.style.opacity);
+});
+*/
